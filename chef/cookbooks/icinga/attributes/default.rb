f7357a2e89401948d2a993019d1e4eb03ac554c1
Daniel Abraão Silva Costa <dascbh@gmail.com>
2016-06-02 15:39:36 -0300

    final version STABLE

--- chef/cookbooks/icinga/attributes/default.rb
+++ chef/cookbooks/icinga/attributes/default.rb
@@ -17,5 +17,5 @@
 #*********************************************************************************
 
 default[:icinga2][:base_pkgs] = [ 'wget', 'icinga2', 'mysql-client', 'mysql-server', 'icinga2-ido-mysql' ]
-default[:icinga_web2][:base_pkgs] = [ 'php5', 'php5-cli', 'php5-xmlrpc', 'php5-json', 'php5-xmlrpc', 'php5-gd', 'php5-ldap', 'php5-imagick'
+default[:icinga_web2][:base_pkgs] = [ 'php5', 'php5-cli', 'php5-xmlrpc', 'php5-json', 'php5-xmlrpc', 'php5-gd', 'php5-ldap', 'php5-imagick',
 									'php5-pgsql', 'php5-intl', 'php5-common', 'php5-mysql', 'icingaweb2', 'nagios-plugins' ]
