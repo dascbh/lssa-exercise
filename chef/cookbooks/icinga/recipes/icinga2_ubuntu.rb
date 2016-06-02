0529c9873e8e52a7f87e354cc6bbbdb3272151e7
Daniel Abraão Silva Costa <dascbh@gmail.com>
2016-06-01 22:19:23 -0300

    testing other web console

--- chef/cookbooks/icinga/recipes/icinga2_ubuntu.rb
+++ chef/cookbooks/icinga/recipes/icinga2_ubuntu.rb
@@ -55,32 +55,32 @@ execute 'enable ido-mysql' do
   action :run
 end
 
-template '/tmp/icingadb_setup.sql' do
-  source 'icingadb_setup.sql.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-end
+# template '/tmp/icingadb_setup.sql' do
+#   source 'icingadb_setup.sql.erb'
+#   owner 'root'
+#   group 'root'
+#   mode '0644'
+# end
 
-execute 'icinga database setup' do
-  command 'mysql -u root < /tmp/icingadb_setup.sql'
-  action
-end
+# execute 'icinga database setup' do
+#   command 'mysql -u root < /tmp/icingadb_setup.sql'
+#   action
+# end
 
-execute 'load ido-mysql schema' do
-  command 'mysql -u root icinga < /usr/share/icinga2-ido-mysql/schema/mysql.sql'
-  action :run
-end
+# execute 'load ido-mysql schema' do
+#   command 'mysql -u root icinga < /usr/share/icinga2-ido-mysql/schema/mysql.sql'
+#   action :run
+# end
 
-execute 'remove tmp sql files' do
-  command 'rm -f /tmp/*.sql'
-  action :run
-end
+# execute 'remove tmp sql files' do
+#   command 'rm -f /tmp/*.sql'
+#   action :run
+# end
 
 
-service 'icinga2' do
-  action :restart
-end
+# service 'icinga2' do
+#   action :restart
+# end
 
 execute 'enable icinga2 command' do
   command 'icinga2 feature enable command'
